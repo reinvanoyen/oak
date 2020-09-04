3a90467919481f938a45714b7e6dc687373b6a4d
Rein Van Oyen <reinvanoyen@gmail.com>
2020-09-05 00:55:21 +0200

    QueryBuilder progress (select, drop, where, limit, offset)

--- src/Database/QueryBuilder/Compiler/Compiler.php
+++ src/Database/QueryBuilder/Compiler/Compiler.php
@@ -5,23 +5,149 @@ namespace Oak\Database\QueryBuilder\Compiler;
 use Oak\Contracts\Database\QueryBuilder\Compiler\CompilerInterface;
 use Oak\Database\QueryBuilder\QueryBuilder;
 
+/**
+ * Class responsible for compiling a QueryBuilder instance to
+ * a query in string format and its parameters, ready to be used by a connection.
+ * 
+ * @package Oak
+ * @author Rein Van Oyen <reinvanoyen@gmail.com>
+ */
 class Compiler implements CompilerInterface
 {
-    private $query;
     
-    public function compile(QueryBuilder $queryBuilder): string
+    /**
+     * @var string $query
+     */
+    private $query = '';
+
+    /**
+     * @var array $parameters
+     */
+    private $parameters = [];
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     */
+    public function compile(QueryBuilder $queryBuilder)
     {
-        $this->query = '';
+        $this->reset();
         
         $table = $queryBuilder->getTable();
-        $select = $queryBuilder->getSelect();
         
-        if ($select) {
+        if ($select = $queryBuilder->getSelect()) {
+            
+            $selectValues = [];
+            foreach ($select as $expr) {
+                $selectValues[] = $expr->getValue();
+            }
+            
             $this->query .= 'SELECT ';
-            $this->query .= implode(',', $select);
+            $this->query .= implode(', ', $selectValues);
             $this->query .= ' FROM '.$table;
+            
+            $this->compileWhere($queryBuilder->getWhere());
+            $this->compileLimitOffset($queryBuilder->getLimit(), $queryBuilder->getOffset());
+            
+        } else if ($drop = $queryBuilder->getDrop()) {
+
+            $this->query .= 'DROP TABLE '.$table;
+        }
+    }
+
+    /**
+     * @param array $where
+     */
+    private function compileWhere(array $where)
+    {
+        $this->compileConditions($where, 'WHERE');
+    }
+
+    /**
+     * @param $limit
+     * @param $offset
+     */
+    private function compileLimitOffset($limit, $offset)
+    {
+        if ($limit) {
+            $this->query .= ' LIMIT '.$limit->getValue();
+            $this->addParameters($limit->getBindings());
+            
+            if ($offset) {
+                $this->query .= ' OFFSET '.$offset->getValue();
+                $this->addParameters($offset->getBindings());
+            }
+        }
+    }
+
+    /**
+     * @param array $conditions
+     * @param string $keyword
+     */
+    private function compileConditions(array $conditions, string $keyword)
+    {
+        if (!count($conditions)) {
+            return;
+        }
+
+        $this->query .= ' '.$keyword.' ';
+
+        $i = 0;
+        foreach ($conditions as $condition) {
+            $this->compileCondition($condition, ($i === 0));
+            $i++;
+        }
+    }
+
+    /**
+     * @param array $condition
+     * @param bool $isFirst
+     */
+    private function compileCondition(array $condition, bool $isFirst = true)
+    {
+        $expr1 = $condition[0];
+        $operator = $condition[1];
+        $expr2 = $condition[2];
+
+        if (! $isFirst) {
+            $this->query .= ' AND ';
         }
         
+        $this->query .= $expr1->getValue().' '.$operator.' '.$expr2->getValue();
+        
+        $this->addParameters($expr1->getBindings());
+        $this->addParameters($expr2->getBindings());
+    }
+
+    /**
+     * 
+     */
+    private function reset()
+    {
+        $this->query = '';
+        $this->parameters = [];
+    }
+    
+    /**
+     * @return string
+     */
+    public function getQuery(): string
+    {
         return $this->query;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param array $parameters
+     */
+    public function addParameters(array $parameters)
+    {
+        $this->parameters = array_merge($this->parameters, $parameters);
     }
 }
--- src/Database/QueryBuilder/QueryBuilder.php
+++ src/Database/QueryBuilder/QueryBuilder.php
@@ -3,8 +3,14 @@
 namespace Oak\Database\QueryBuilder;
 
 use Oak\Contracts\Database\QueryBuilder\Compiler\CompilerInterface;
-use Oak\Contracts\Database\QueryBuilder\StatementInterface;
+use Oak\Contracts\Database\QueryBuilder\ExpressionInterface;
 
+/**
+ * This class provides a dynamic PHP API around building queries.
+ *
+ * @package Oak
+ * @author Rein Van Oyen <reinvanoyen@gmail.com>
+ */
 class QueryBuilder
 {
     /**
@@ -16,6 +22,26 @@ class QueryBuilder
      * @var array $select
      */
     private $select = [];
+
+    /**
+     * @var array $where
+     */
+    private $where = [];
+
+    /**
+     * @var ExpressionInterface $limit
+     */
+    private $limit;
+    
+    /**
+     * @var ExpressionInterface $offset
+     */
+    private $offset;
+
+    /**
+     * @var bool $drop
+     */
+    private $drop;
 
     /**
      * @var CompilerInterface $compiler
@@ -48,13 +74,54 @@ class QueryBuilder
     public function select($columns = []): QueryBuilder
     {
         if (! count($columns)) {
-            $columns = ['*'];
+            $this->select = [new Raw('*'),];
         }
         
-        $this->select = $columns;
+        foreach ($columns as $column) {
+            $this->select[] = $this->createExpression($column, true);
+        }
+        
         return $this;
     }
 
+    /**
+     * @return QueryBuilder
+     */
+    public function drop(): QueryBuilder
+    {
+        $this->drop = true;
+        return $this;
+    }
+    
+    public function where($expr1, string $operator, $expr2): QueryBuilder
+    {
+        $this->where[] = [
+            $this->createExpression($expr1, true),
+            $operator,
+            $this->createExpression($expr2),
+        ];
+        return $this;
+    }
+
+    /**
+     * @param $limit
+     * @return QueryBuilder
+     */
+    public function limit($limit): QueryBuilder
+    {
+        $this->limit = $this->createExpression($limit);
+        return $this;
+    }
+
+    /**
+     * @param $offset
+     * @return QueryBuilder
+     */
+    public function offset($offset): QueryBuilder
+    {
+        $this->offset = $this->createExpression($offset);
+        return $this;
+    }
 
     /**
      * @return string
@@ -62,6 +129,22 @@ class QueryBuilder
     public function getTable(): string
     {
         return $this->table;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function getDrop(): bool
+    {
+        return $this->drop;
+    }
+
+    /**
+     * @return array
+     */
+    public function getWhere(): array 
+    {
+        return $this->where;
     }
     
     /**
@@ -73,23 +156,76 @@ class QueryBuilder
     }
 
     /**
-     * @param $statement
-     * @return StatementInterface
+     * @return ExpressionInterface|null
      */
-    protected function createStatement($statement): StatementInterface
+    public function getLimit(): ?ExpressionInterface
     {
-        if ($statement instanceof StatementInterface) {
-            return $statement;
-        }
-        
-        return new Raw('?', [$statement, ]);
+        return $this->limit;
     }
 
     /**
+     * @return ExpressionInterface|null
+     */
+    public function getOffset(): ?ExpressionInterface
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @param $statement
+     * @param bool $prefix
+     * @return ExpressionInterface
+     */
+    protected function createExpression($expr, $prefix = false): ExpressionInterface
+    {
+        if ($expr instanceof ExpressionInterface) {
+            return $expr;
+        }
+        
+        return ($prefix ? new Raw($this->prefix($expr)) : new Raw('?', [$expr,]));
+    }
+
+    /**
+     * @param string $column
      * @return string
      */
-    public function build(): string
+    protected function prefix(string $column): string
     {
-        return $this->compiler->compile($this);
+        $table = $this->table;
+        $parts = explode('.', $column, 2);
+        
+        if (count($parts) > 1) {
+            $table = $parts[0];
+            $column = $parts[1];
+        }
+        
+        return $this->quote($table).'.'.$this->quote($column);
+    }
+
+    /**
+     * @param string $statement
+     * @return string
+     */
+    final protected function quote(string $statement)
+    {
+        return '`'.$statement.'`';
+    }
+    
+    /**
+     * 
+     */
+    public function build()
+    {
+        $this->compiler->compile($this);
+    }
+    
+    public function getQuery()
+    {
+        return $this->compiler->getQuery();
+    }
+
+    public function getParameters()
+    {
+        return $this->compiler->getParameters();
     }
 }
